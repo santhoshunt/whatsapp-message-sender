f1a402e1d422c906bef48ed334f336fc34e65563
Santhosh <50547679+santhoshunt@users.noreply.github.com>
2020-08-07 21:28:54 +0530

    Delete automsgwhatsapp.py

--- automsgwhatsapp.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-time.sleep(3)
-for i in range(200):
-    pyautogui.write("IDIOT")
-    pyautogui.press('\n')
-    if input()=='esc':
-        exit()
-pyautogui.press('enter')
