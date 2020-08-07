d9e52b7df3bb6044a6c681f75fa35080ead68f48
Santhosh <50547679+santhoshunt@users.noreply.github.com>
2020-08-07 21:43:51 +0530

    Add files via upload

---
+++ autowhatmsgsender.py
@@ -0,0 +1,11 @@
+#importing the module which is used to send messages
+
+import pywhatkit
+s=int(input("Enter the user's  number :"))#getting the number
+hours=int(input("Enter the hour (in 24 hr format) :"))
+min=int(input("Enter the minute :"))
+msg=input("Enter the message you wanna send :").strip()
+#the code used to send messgaes
+pywhatkit.sendwhatmsg("+91"+str(s),msg,hours,min)
+#the message will be sent to the user in 30 seconds python opens whatsappweb
+#the message will be sent @6.10 PM
